b64f516e9b86474427e63ca8a1aaf230665a1043
benbenz101 <bensonbenzene@gmail.com>
2022-07-20 13:04:07 +0100

    finished form validation using html, css and javascript

---
+++ style.js
@@ -0,0 +1,28 @@
+// function validateForm() {
+//   let x = document.forms["signinForm"]["Email"].value;
+//   if (x == "") {
+//     alert("email must be filled out");
+//     return false;
+//   }
+// }
+
+//  function validate() {
+
+//          if( document.myForm.email.value == "" ) {
+//             alert( "Please provide your email!" );
+//             document.myForm.email.focus() ;
+//             return false;
+//          }
+
+// function validateEmail() {
+//   var emailID = document.myForm.Email.value;
+//   atpos = emailID.indexOf("@");
+//   dotpos = emailID.lastIndexOf(".");
+
+//   if (atpos < 1 || dotpos - atpos < 2) {
+//     alert("Please enter correct email ID");
+//     document.myForm.EMail.focus();
+//     return false;
+//   }
+//   return true;
+//}
